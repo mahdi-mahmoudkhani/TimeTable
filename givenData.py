8a32050536c4505e4d36f7d1c609b4f4c1e05365
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2025-01-02 22:21:58 +0330

    feat(data): add givenData.py to store sample data for instructors, classrooms, and time slots
    - Created `givenData.py` to define sample data for instructors, classrooms, and time slots.
    - The file includes instructor lists associated with courses, a list of available classrooms, and predefined time slots.
    - This data will be used for testing and initializing the CSP problem in the `State` class.

---
+++ givenData.py
@@ -0,0 +1,12 @@
+instructors = {
+    "AI": ["Dr.Moosavi", "Dr.Shahabi"],
+    "Physics": ["Dr.Pouzesh"],
+    "Chemistry": ["Dr.Fathi"],
+    "Music": ["Dr.Shokoohi", "Dr.Mortazavi"],
+    "Cinema": ["Dr.Mortazavi", "Dr.Khosravani"],
+    "Algebra": ["Dr.Pourbagheri"]
+}
+
+classrooms = ["Room1", "Room2", "Room3"]
+
+time_slots = ["9:00-10:00", "10:00-11:00", "11:00-12:00"]
