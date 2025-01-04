4a7bcac61126dc05012a8c65d7873ebf537b083d
Mahsa <haghnevismahsa@gmail.com>
2025-01-04 16:06:49 +0330

    feat: add initial structure for forward checking function
    - Created the forward_checking function with basic arguments and structure.
    - Unpacks the input value (time-room and instructor).
    - Placeholder implementation; no domain modifications applied yet.

---
+++ forwardChecking.py
@@ -0,0 +1,10 @@
+
+def forward_checking(state , variable , value):
+    '''Forward Checking function to update the domains in the state
+    after assigning a value to a variable. '''
+    
+    #unpacked the value 
+    timeRoom ,  instructor = value
+    
+    
+    return state
