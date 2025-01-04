3b9bf992fe7aa356c3231cafc304f8dfcc803be0
Mahsa <haghnevismahsa@gmail.com>
2025-01-04 16:25:00 +0330

    fix: import State class for managing CSP state
    - Imported State class from state module.

--- forwardChecking.py
+++ forwardChecking.py
@@ -1,4 +1,6 @@
+from state import State
 
+# Forward Checking function
 def forward_checking(state , variable , value):
     '''Forward Checking function to update the domains in the state
     after assigning a value to a variable. '''
@@ -16,7 +18,7 @@ def forward_checking(state , variable , value):
             domain_instructor , domain_timeRoom = domain_value
             if domain_timeRoom == timeRoom or domain_instructor == instructor:
                 continue  
-            newDomain.append(domain_value)
+            newDomain.append(domain_value) 
             
             
         state['domains'][course] = newDomain
