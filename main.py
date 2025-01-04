fc74fd25eaf0c1208c55d8fadbb2a51ce4748849
Mahsa <haghnevismahsa@gmail.com>
2025-01-04 23:25:38 +0330

    feat: add main function to execute backtracking algorithm
    - Implemented the main function to test the backtracking algorithm with a sample state.
    - Integrated the format_Solution function to display the solution in a readable format.
    - Imported required modules: backTracking, format_Solution, and State.
    - Prints the scheduling solution if found or an appropriate message if no solution exists.
    - Ensures proper execution flow by checking __name__ == "__main__".

---
+++ main.py
@@ -0,0 +1,26 @@
+from backTracking import backTracking
+from format import format_Solution
+from State import State
+
+def main():
+    # Sample input state
+    state = {
+        "domains": {
+            "AI": [("9:00-10:00 Room1", "Dr.Moosavi"), ("10:00-11:00 Room2", "Dr.Shahabi")],
+            "Physics": [("9:00-10:00 Room1", "Dr.Pouzesh"), ("10:00-11:00 Room3", "Dr.Pouzesh")],
+            "Chemistry": [("10:00-11:00 Room2", "Dr.Fathi"), ("9:00-10:00 Room3", "Dr.Karimi")],
+        }
+    }
+
+    # Run the backtracking algorithm
+    solution = backTracking(state)
+     # Print the solution in a formatted table
+    if solution:
+        print("Solution Found:")
+        print(format_Solution(solution))
+    else:
+        print("No solution found.")
+        
+
+if __name__ == "__main__":
+    main()
