19976e6f3ac325890b9106610d8752be2478763e
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2025-01-05 11:41:24 +0330

    refactor(heuristic): update lcv function to integrate with State class
    - Refactored `lcv` function to accept a `State` object as input instead of a dictionary.
    - Adjusted logic to iterate over `state.domains` keys to evaluate constraints for a course's domain values.
    - Ensures compatibility with the `State` class by modifying variable unpacking and comparisons.
    - Retains functionality to select the value that imposes the least constraints on other variables.

--- lcv.py
+++ lcv.py
@@ -1,50 +1,33 @@
+from State import State
 
-def lcv(state , variable):
+
+def lcv(state: State, variable):
     '''
     Least Constraining Value (LCV) heuristic to select the best value for a variable.
     Returns:
         tuple: The value from the domain of the variable that causes the least constraints.
-    
+
     '''
-    
     valueConstraints = []
-    
     # get the domain of the variable
-    domain = state['domains'][variable]
-    
+    domain = state.domains[variable]
+
     for value in domain:
         # initialize the constraints to 0
         constraints = 0
         # unpack the value
-        timeRoom , instructor = value
-        
-        for otherVar in state['domains']:
+        instructor, roomTime = value
+        for otherVar in state.domains.keys():
             if otherVar == variable:
                 continue
-            
-            for otherValue in state['domains'][otherVar]:
-                otherTimeRoom , otherInstructor = otherValue
-                if otherTimeRoom == timeRoom or otherInstructor == instructor:
+            for otherValue in state.domains[otherVar]:
+                otherInstructor, otherRoomTime = otherValue
+                if otherRoomTime == roomTime or otherInstructor == instructor:
                     constraints += 1
-                    
-        # append the value and its constraints to the list   
-        valueConstraints.append((value , constraints))
-    #sort the values based on the lowest constraints
-    valueConstraints.sort(key = lambda x : x[1] )
-    #print(valueConstraints)
+        # append the value and its constraints to the list
+        valueConstraints.append((value, constraints))
+    # sort the values based on the lowest constraints
+    valueConstraints.sort(key=lambda x: x[1])
 
-    return valueConstraints[0][0] 
-
-# # Sample state for testing
-# state = {
-#     "domains": {
-#         "AI": [("9:00-10:00 Room1", "Dr.Moosavi"), ("10:00-11:00 Room2", "Dr.Shahabi")],
-#         "Physics": [("9:00-10:00 Room1", "Dr.Pouzesh"), ("10:00-11:00 Room2", "Dr.Pouzesh")],
-#         "Chemistry": [("10:00-11:00 Room2", "Dr.Fathi")],
-#     }
-# }
-
-# selected_value = lcv(state, "AI")
-# print(f"Selected value by LCV: {selected_value}")
-            
-           
+    # print(valueConstraints)
+    return valueConstraints[0][0]
